6e45d26299f525eb1ea01a901faf81d56bc9af3f
tadsareveena <tadsareveenav@gmail.com>
2022-07-11 11:55:18 +0530

    second commit

---
+++ Assignment13/Data/EmployeeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment13.Models;
+
+namespace Assignment13.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext (DbContextOptions<EmployeeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment13.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Assignment13/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment13.Models
+{
+
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public int EId { get; set; }
+        public string EName { get; set; }
+
+        public string EDesign { get; set; }
+
+        public DateTime? EDOJ { get; set; }
+
+    }
+}
+
